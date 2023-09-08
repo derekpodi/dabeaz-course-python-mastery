e01953a8753629194397efc2ec49b15fcdb17ac3
Derek Podimatis <89597550+derekpodi@users.noreply.github.com>
2023-09-07 23:03:30 -0700

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,16 @@
+# sample.py
+
+from logcall import logged
+from logcall import logformat
+
+@logged
+def add(x,y):
+    return x+y
+
+@logged
+def sub(x,y):
+    return x-y
+
+@logformat('{func.__code__.co_filename}:{func.__name__}')
+def mul(x,y):
+    return x*y
